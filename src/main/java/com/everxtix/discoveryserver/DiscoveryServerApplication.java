8ad8efd946c04e272b9204abcf7d7f7161254652
Jesus Duran Huancas <jesusgonzaloduran@gmail.com>
2020-10-29 16:08:30 -0500

    fix attemp 3

--- src/main/java/com/everxtix/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/com/everxtix/discoveryserver/DiscoveryServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
@@ -11,5 +12,7 @@ public class DiscoveryServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
     }
+
+
 
 }
